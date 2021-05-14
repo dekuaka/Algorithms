45f6469499ec7fb6bd5505a288064370b77fb6ec
unknown <lqxc97@hotmail.com>
2021-05-13 21:21:04 -0400

    Test

--- src/TestCase/Test.java
+++ src/TestCase/Test.java
@@ -1,9 +1,11 @@
 package TestCase;
-
+import java.util.Scanner;
+import edu.princeton.cs.algs4.StdRandom;
 public class Test {
 	public static void main(String[] args) {
-		String a  = new String("abc");
-		a = "bcd";
-		System.out.println(a);
+		//Scanner sc = new Scanner(System.in);
+		//System.out.print("Please enter a string:");
+		//System.out.print("Your input is:" + sc.next());
+		System.out.print(args[0]);
 	}
 }
